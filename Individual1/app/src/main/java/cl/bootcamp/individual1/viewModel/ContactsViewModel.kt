78e837a8bb1b846222eba85b942f94c5b207eb45
Javier Castillo <jkttzars@gmail.com>
2024-09-26 18:22:44 -0300

    end and finish excesice 1

--- Individual1/app/src/main/java/cl/bootcamp/individual1/viewModel/ContactsViewModel.kt
+++ Individual1/app/src/main/java/cl/bootcamp/individual1/viewModel/ContactsViewModel.kt
@@ -1,4 +1,26 @@
 package cl.bootcamp.individual1.viewModel
 
-class ContactsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cl.bootcamp.individual1.model.Contacts
+import cl.bootcamp.individual1.repository.ContatcsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ContactsViewModel @Inject constructor(private val repository: ContatcsRepository) : ViewModel() {
+
+    fun insertContact(contact: Contacts) = viewModelScope.launch {
+        repository.insertContact(contact)
+    }
+
+    fun updateContact(contact: Contacts) = viewModelScope.launch {
+        repository.updateContact(contact)
+    }
+
+    fun deleteContact(contact: Contacts) = viewModelScope.launch {
+        repository.deleteContact(contact)
+    }
+
 }
