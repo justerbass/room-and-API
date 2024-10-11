ac4331e954eac636ac7c94ee4493983bdda8f130
Javier Castillo <jkttzars@gmail.com>
2024-10-11 09:12:57 -0300

    start excercise 6

---
+++ Individual6/app/src/main/java/cl/bootcamp/individual6/MainActivity.kt
@@ -0,0 +1,27 @@
+package cl.bootcamp.individual6
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cl.bootcamp.individual6.ui.theme.Individual6Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Individual6Theme {
+
+            }
+        }
+    }
+}
+
---
+++ Individual6/app/src/main/java/cl/bootcamp/individual6/MiApp.kt
@@ -0,0 +1,8 @@
+package cl.bootcamp.individual6
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MiApp : Application(){
+}
